e03f89bde8491ccba55661fe93219ad954de766b
buarg <hector_buarg@hotmail.com>
2020-11-04 15:22:53 +0100

    Agregar archivos de proyecto.

---
+++ Practica 1 - 4/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_1___4 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            Button nuevo = new Button();
+            SuspendLayout();
+            nuevo.Location = new System.Drawing.Point(350, 73);
+            nuevo.Name = "nuevo";
+            nuevo.Size = new System.Drawing.Size(102, 53);
+            nuevo.TabIndex = 2;
+            nuevo.Text = "Nuevo";
+            nuevo.UseVisualStyleBackColor = true;
+            nuevo.Click += new System.EventHandler(this.nuevo_Click);
+            this.Controls.Add(nuevo);
+            ResumeLayout(false);
+        }
+
+        private void nuevo_Click(object sender, EventArgs e) {
+            string message = "Hola";
+            string caption = "";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result = MessageBox.Show(message, caption, buttons);
+        }
+    }
+}
